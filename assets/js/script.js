86d1cd6722d5a25f3fcfae7b7bceca29bf279e44
Daniel Beckett <dan-beckett@hotmail.co.uk>
2024-11-25 12:39:52 +0000

    Add: global variables and event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 // Game state, defaults to off and is controlled using the buttons beneath the game text input box
 let gameState = "off";
+// Game text input box
+let typingArea = document.getElementById('typing-area');
+// Start button
+let startButton = document.getElementById('start-button');
+// Stop button
+let stopButton = document.getElementById('stop-button');
+// Retry button
+let retryButton = document.getElementById('retry-button');
+
+// Event Listeners
+startButton.addEventListener('click', startGame);
+stopButton.addEventListener('click', stopGame);
+retryButton.addEventListener('click', retryGame);
+typingArea.addEventListener('input', checkInput);
+
 
 // Function to generate a random string for the user to type. No more than ten words at a time.
 // Using the API from https://random-word.ryanrk.com/
